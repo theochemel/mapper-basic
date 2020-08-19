58471d6b185c0c5ccfd413e6e833d5f6aa6b8db7
theochemel <theo@chemel.net>
2020-08-19 17:57:06 -0400

    Initial Commit

---
+++ mapper-basic/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  mapper-basic
+//
+//  Created by Theo Chemel on 8/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mapper-basic/mapper_basicApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapper_basicApp.swift
+//  mapper-basic
+//
+//  Created by Theo Chemel on 8/19/20.
+//
+
+import SwiftUI
+
+@main
+struct mapper_basicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
